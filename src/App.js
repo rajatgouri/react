933a677f2fa1b986474ce419525ab49d11e407e1
rajatgouri020 <rajatgouri020@gmail.com>
2021-08-26 14:20:48 +0530

    made changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import Form from "./Form";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [users, setUsers] = useState([]);
+  const handleFormSubmit = (data) => {
+    setUsers([...users, data]);
+  };
+
+  const onDeleteUser = (index) => {
+    setUsers([]);
+    let userList = users;
+    userList.splice(index, 1);
+    setTimeout(() => {
+      setUsers(userList);
+    }, 100);
+  };
+
+  // one minute on a phone call
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello CodeSandbox</h1>
+      <div className="row">
+        <div className="col-6">
+          <Form handleFormSubmit={(data) => handleFormSubmit(data)} />
+        </div>
+        <div className="col-6">
+          {users.map((user, index) => {
+            return (
+              <div className="row" key={index}>
+                <div className="col-10">
+                  {user.firstName} {user.lastName} {user.email}
+                </div>
+                <div className="col-2">
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => onDeleteUser(index)}
+                  >
+                    X
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
